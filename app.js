ffc6dcb44227e46c730accf577f0b4b8e4326c42
luchenqun <luchenqun@qq.com>
2019-04-19 09:34:00 +0800

    支持允许跨域

--- app.js
+++ app.js
@@ -37,6 +37,15 @@ function downloadFile(uri, filename, callback) {
     }
 }
 
+//设置允许跨域访问该服务.
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next();
+});
+
 app.get("/", function (req, res) {
     let url = req.query.url;
     let options = {
